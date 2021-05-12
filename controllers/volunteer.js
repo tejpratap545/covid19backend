1462750f74e13d4c6a7f8c149869576770f8f8b3
Tejpratap <tejpratapsingh545@outlook.com>
2021-05-12 21:54:19 +0530

    volunteer create

--- controllers/volunteer.js
+++ controllers/volunteer.js
@@ -58,3 +58,52 @@ exports.destroy = async function (req, res) {
     res.status(500).json({ message: error.message });
   }
 };
+
+exports.upload = async function (req, res) {
+  const volunteer = await Volunteer.findById(req.body.volunteer);
+  if (!volunteer) {
+    res.status(401).json({ msg: "No Such User " });
+    return;
+  }
+
+  let volunteers = [];
+  const tempFile = req.files.resource.tempFilePath;
+  fs.createReadStream(tempFile)
+    .pipe(csv.parse())
+    .on("error", (error) => console.error(error))
+    .on("data", async (row) => {
+      let [name, mobileNumber, email, password, role, cities] = row;
+      password = await bcrypt.hash(password, 10);
+
+      if (name != "" && name != "Name") {
+        let citiesID = [];
+        cities = cities.split("&").forEach(async (city) => {
+          const c = await City.findOne({ name: city });
+          if (c) {
+            citiesID.push(c.id);
+          }
+        });
+
+        volunteers.push({
+          email: email,
+          mobileNumber: mobileNumber,
+          email: email,
+          encryptedPassword: password,
+          city: cities,
+          createdBy: volunteer.id,
+          superAdmin: volunteer.id,
+          isActive: true,
+          role: "volunteer",
+        });
+      }
+
+      await Volunteer.create(volunteers);
+    })
+    .on("end", (rowCount) => console.log(`Parsed ${rowCount} rows`));
+
+  fs.unlink(tempFile, (err) => {
+    if (err) throw err;
+    console.log(`${tempFile}was deleted`);
+  });
+  res.status(200).json({ msg: "data is successfully imported " });
+};
